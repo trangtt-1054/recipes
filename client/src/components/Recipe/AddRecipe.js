e26d5be90dc8f8fa8b095cadbb21c6dbc2699b03
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2020-03-14 14:54:27 +0700

    update cache

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Mutation } from "react-apollo";
-import { ADD_RECIPE } from "../../queries";
+import { ADD_RECIPE, GET_ALL_RECIPES } from "../../queries";
 import Error from "../Error";
 import { withRouter } from 'react-router-dom';
 
@@ -43,9 +43,33 @@ class AddRecipe extends React.Component {
       await this.props.refetch();
       this.props.history.push('/');
     });
-    // this.props.history.push('/');
-    
+    // this.props.history.push('/'); 
   };
+
+  updateCache = (cache, {data: { addRecipe }}) => {
+    //manually update getAllRecipes query
+    //console.log(cache, data); 
+    /*{InMemoryCache:{}} 
+    trong đấy có 1 cái data object chứa tất cả các recipe bao gồm cả cái mình vừa add
+    data chính là cái data trả về khi thực hiện mutation add recipe, có dạng {addRecipe:{}}
+    when we want to update query manually, we will take the data and add to something in this cache object, cụ thể ở đây là $ROOT_QUERY and ROOT_MUTATION
+    */
+   const { getAllRecipes } = cache.readQuery({ query: GET_ALL_RECIPES })
+   //readQuery: Reads a graphQL quẻy from the root query id
+   console.log('from cache', getAllRecipes);
+   console.log('from data', addRecipe);
+
+   cache.writeQuery({
+     query: GET_ALL_RECIPES,
+     data: {
+       getAllRecipes: [addRecipe, ...getAllRecipes]
+       /* addRecipe phải để lên đầu array vì đấy là cái mới nhất.
+       nếu mà viết getAllRecipes.concat([addRecipe] thì nó sẽ nhảy xuống cuối, phải refresh page lần nữa mới lên đầu)
+       */
+     }
+   })
+   //readQuery rồi writeQuery còn gọi là flow of optimistic UI :-?
+  }
 
   render() {
     const { name, category, description, instructions, username } = this.state;
@@ -53,10 +77,14 @@ class AddRecipe extends React.Component {
     return (
       <Mutation
         mutation={ADD_RECIPE}
-        variables={{ name, category, description, instructions, username }}
+        variables={{ name, category, description, instructions, username }
+}
+        update={this.updateCache}
       >
         {(addRecipe, { data, loading, error }) => {
-          //we want to make onSubmit an arrow fn so that it won't be called on page load.
+          /*we want to make onSubmit an arrow fn so that it won't be called on page load.
+          updateCache: sau khi add recipe, mất 1 thời gian mutation mới thực hiện xong nên khi quay về homepage sẽ ko tự động update data mới nhất => phải update manually
+          */
           return (
             <div className="App">
               <h2 className="App">Add Recipe</h2>
